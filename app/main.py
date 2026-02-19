b1618f0e8ce7201902a6b1be9c8efc08cfaf069e
nightfairy <nightfairy5831@gmail.com>
2026-02-19 21:26:40 +0000

    chore:upgrade main.py for cors setting

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="AI Chatbot SaaS")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://ai-chatbot-frontend-virid.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
